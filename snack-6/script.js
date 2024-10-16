36e9dd2f9bc5cae7ebd9efdf4bcd9ad8746d0ba6
feffe1702 <fpm.asd@gmail.com>
2024-10-16 16:11:52 +0200

    [DONE] complete ex6

--- snack-6/script.js
+++ snack-6/script.js
@@ -12,3 +12,13 @@ const zucchine = [
 ];
 
 // A partire dall'array fornito, crea due array. Uno con le zucchine più lunghe di almeno 15cm. L'altro con le restanti.
+
+let zucchineLunghe = zucchine.filter((obj) => {
+  return obj.length > 15;
+});
+
+let zucchinePiccole = zucchine.filter((obj) => {
+  return obj.length < 15;
+})
+
+console.log(zucchineLunghe , zucchinePiccole);
