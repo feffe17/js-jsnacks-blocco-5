655c6052cb9052f02464583eacf1fe49f29848b9
feffe1702 <fpm.asd@gmail.com>
2024-10-16 16:24:41 +0200

    [DONE] complete ex8

--- snack-8/script.js
+++ snack-8/script.js
@@ -21,3 +21,7 @@ const students = [
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
+
+let classe = students.find((obj) => obj.name === "Marco Lanci").class;
+
+console.log(classe);
