9793edbad1f9cbcdeaf1544eec404a5099aed99d
feffe1702 <fpm.asd@gmail.com>
2024-10-16 16:01:14 +0200

    [DONE] complete ex4

--- snack-4/script.js
+++ snack-4/script.js
@@ -7,3 +7,8 @@ const posts = [
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+
+
+let autori = posts.map((obj) => obj.author);
+
+console.log(autori);
