0e8935deb6373969a0fd38a3dfb005abfa93c143
feffe1702 <fpm.asd@gmail.com>
2024-10-16 15:45:26 +0200

    [DONE] complete ex 3

--- snack-3/script.js
+++ snack-3/script.js
@@ -2,3 +2,11 @@ const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
+
+let incrementati = [];
+
+numbers.map((number) => {
+    incrementati.push(number + 1);
+})
+
+console.log(incrementati);
