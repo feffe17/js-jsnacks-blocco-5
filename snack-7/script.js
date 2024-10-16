1de60928d250eee798d03a5ff42a6eb9234eb4cf
feffe1702 <fpm.asd@gmail.com>
2024-10-16 16:17:29 +0200

    [DONE] complete ex7

--- snack-7/script.js
+++ snack-7/script.js
@@ -21,3 +21,7 @@ const students = [
 
 // Recupera i dati dello studente con id 2
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
+
+let studente = students.find(Object => Object.id === 2);
+
+console.log(studente);
